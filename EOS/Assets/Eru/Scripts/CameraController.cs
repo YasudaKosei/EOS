f41bc9efc87c0fa056fb3ec5d07f45581816ad90
Eru <eru.gamecreater@gmail.com>
2023-07-06 14:55:33 +0900

    カメラ調整

--- EOS/Assets/Eru/Scripts/CameraController.cs
+++ EOS/Assets/Eru/Scripts/CameraController.cs
@@ -2,36 +2,28 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform target;  // 追従する対象（プレイヤー）
+    public Transform player; // プレイヤーのTransform
+    public float distance = -6f; // プレイヤーからの距離
+    public float height = 3f; // プレイヤーからの高さ
 
-    public float rotationSpeed = 5f;  // カメラの回転速度
+    private Vector3 offset; // カメラのオフセット
 
-    private Vector3 offset;  // カメラとプレイヤーの位置オフセット
-
-    private void Start()
+    void Start()
     {
-        offset = transform.position - target.position;  // 初期オフセットを計算
+        // カメラの初期位置を設定
+        offset = new Vector3(0f, height, -distance);
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
     }
 
-    private void LateUpdate()
+    void LateUpdate()
     {
-        // プレイヤーの位置に合わせてカメラの位置を更新
-        transform.position = target.position + offset;
+        // プレイヤーの回転に合わせてカメラを移動・回転させる
+        Quaternion playerRotation = player.rotation;
+        Quaternion cameraRotation = Quaternion.Euler(0f, playerRotation.eulerAngles.y, 0f);
+        Vector3 desiredPosition = player.position + (cameraRotation * offset);
 
-        // カメラがプレイヤーを見るように向きを設定
-        transform.LookAt(target);
-
-        // キー入力に応じてカメラを回転させる
-        float horizontalInput = Input.GetAxis("Horizontal");
-        if (horizontalInput != 0f)
-        {
-            RotateCameraAroundPlayer(horizontalInput);
-        }
-    }
-
-    private void RotateCameraAroundPlayer(float direction)
-    {
-        // カメラをプレイヤーを中心に回転させる
-        transform.RotateAround(target.position, Vector3.up, direction * rotationSpeed);
+        transform.position = desiredPosition;
+        transform.LookAt(player.position);
     }
 }
