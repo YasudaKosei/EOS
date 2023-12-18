ecdcce7a1c49dfb69f93cf9f6bc7c751e0a5ec54
YasudaKosei <127881638+YasudaKosei@users.noreply.github.com>
2023-12-18 20:50:06 +0900

    ゴール仮作成

---
+++ EOS/Assets/Cream/Script/GoalManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GoalManager : MonoBehaviour
+{
+    [SerializeField] GameObject Result;
+
+    [SerializeField] Text ResultTime;
+
+    [SerializeField] Text TimeText;
+
+    DebugStart debugStart;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("CLEAR");
+            Stop.stopFlg = true;
+            ResultTime.text = TimeText.text;
+            BGMManager.instance.StopBGM();
+            Result.SetActive(true);
+            Invoke("MoveTitle", 5f);
+        }
+    }
+
+    void MoveTitle()
+    {
+        debugStart = FindObjectOfType<DebugStart>();
+        debugStart.MoveTitleScene();
+    }
+
+}
