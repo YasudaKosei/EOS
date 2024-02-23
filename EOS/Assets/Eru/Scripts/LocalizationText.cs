1869e21e882e00fad60c194635511e5bb05af939
Eru <eru.gamecreator@gmail.com>
2024-02-24 00:34:12 +0900

    バグ修正

--- EOS/Assets/Eru/Scripts/LocalizationText.cs
+++ EOS/Assets/Eru/Scripts/LocalizationText.cs
@@ -7,10 +7,13 @@ public class LocalizationText : MonoBehaviour
     private Text screenMode;
 
     [SerializeField]
-    private Text screenModeLabel, resolution, x_sensitivity, y_sensitivity;
+    private Text screenModeLabel, resolution, language, languageLabel, x_sensitivity, y_sensitivity;
 
     [SerializeField]
-    private Dropdown dis;
+    private Dropdown screenDD;
+
+    [SerializeField]
+    private Dropdown languageDD;
 
     [SerializeField, Header("キーボード")]
     private Text k_up;
@@ -37,11 +40,16 @@ public class LocalizationText : MonoBehaviour
         //ディスプレイ設定
         screenMode.text = "スクリーンモード";
         resolution.text = "解像度";
+        language.text = "言語";
+        languageDD.options[0].text = "日本語";
+        languageDD.options[1].text = "英語";
+        if (languageDD.value == 0) languageLabel.text = "日本語";
+        else languageLabel.text = "英語";
         x_sensitivity.text = "X軸(左右)感度";
         y_sensitivity.text = "Y軸(上下)感度";
-        dis.options[0].text = "フルスクリーン";
-        dis.options[1].text = "ウィンドウ";
-        if (dis.value == 0) screenModeLabel.text = "フルスクリーン";
+        screenDD.options[0].text = "フルスクリーン";
+        screenDD.options[1].text = "ウィンドウ";
+        if (screenDD.value == 0) screenModeLabel.text = "フルスクリーン";
         else screenModeLabel.text = "ウィンドウ";
 
         //キーボード設定
@@ -64,11 +72,16 @@ public class LocalizationText : MonoBehaviour
         //ディスプレイ設定
         screenMode.text = "ScreenMode";
         resolution.text = "Resolution";
+        language.text = "Language";
+        languageDD.options[0].text = "Japanese";
+        languageDD.options[1].text = "English";
+        if (languageDD.value == 0) languageLabel.text = "Japanese";
+        else languageLabel.text = "English";
         x_sensitivity.text = "SensitivityX";
         y_sensitivity.text = "SensitivityY";
-        dis.options[0].text = "FullScreen";
-        dis.options[1].text = "Window";
-        if (dis.value == 0) screenModeLabel.text = "FullScreen";
+        screenDD.options[0].text = "FullScreen";
+        screenDD.options[1].text = "Window";
+        if (screenDD.value == 0) screenModeLabel.text = "FullScreen";
         else screenModeLabel.text = "Window";
 
         //キーボード設定
