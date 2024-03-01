40d1d131a5321df439a9cf6fadbd30df06a97290
Eru <eru.gamecreater@gmail.com>
2024-03-01 13:37:16 +0900

    ベットSE追加

--- EOS/Assets/Eru/Scripts/StageGimmick/JumpTrap.cs
+++ EOS/Assets/Eru/Scripts/StageGimmick/JumpTrap.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class JumpTrap : MonoBehaviour
 {
-    [SerializeField,Header("ジャンプ力")]
+    [SerializeField, Header("ジャンプ力")]
     private float jumpPower = 5f;
 
-    [SerializeField,Header("プレイヤー指定")]
+    [SerializeField, Header("プレイヤー指定")]
     private PlayerType playerType;
+
+    [SerializeField, Header("SE")]
+    private AudioClip clip;
+
+    private new AudioSource audio;
 
     private enum PlayerType
     {
@@ -22,14 +27,26 @@ public class JumpTrap : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         //対象のプレイヤーか検知
-        if (playerType != PlayerType.none && other.gameObject.tag != playerType.ToString()) return; 
-        if (other.gameObject.TryGetComponent<Rigidbody>(out Rigidbody rb)) rb.AddForce(jumpPower * Vector3.up, ForceMode.Impulse);
+        if (playerType != PlayerType.none && other.gameObject.tag != playerType.ToString()) return;
+        if (other.gameObject.TryGetComponent<Rigidbody>(out Rigidbody rb))
+        {
+            rb.AddForce(jumpPower * Vector3.up, ForceMode.Impulse);
+            audio = other.gameObject.GetComponent<AudioSource>();
+            audio.clip = clip;
+            audio.Play();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         //対象のプレイヤーか検知
         if (playerType != PlayerType.none && collision.gameObject.tag != playerType.ToString()) return;
-        if (collision.gameObject.TryGetComponent<Rigidbody>(out Rigidbody rb)) rb.AddForce(jumpPower * Vector3.up, ForceMode.Impulse);
+        if (collision.gameObject.TryGetComponent<Rigidbody>(out Rigidbody rb))
+        {
+            rb.AddForce(jumpPower * Vector3.up, ForceMode.Impulse);
+            audio = collision.gameObject.GetComponent<AudioSource>();
+            audio.clip = clip;
+            audio.Play();
+        }
     }
 }
